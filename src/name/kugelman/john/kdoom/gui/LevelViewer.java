07fe42f5ce5667bba8f78c985eb37b3b7b98b93c
john.kugelman <john.kugelman@8d29ecf9-fe48-0410-a8a8-27ad8145026f>
2008-04-02 04:08:42 +0000

    Allow full -32767 to 32768 extent of map to be explored. Auto-zoom to show full map.

--- src/name/kugelman/john/kdoom/gui/LevelViewer.java
+++ src/name/kugelman/john/kdoom/gui/LevelViewer.java
@@ -24,7 +24,7 @@ public class LevelViewer extends JFrame {
 
         this.level = level;
 
-        this.levelPanel  = new LevelPanel (level, palette);
+        this.levelPanel  = new LevelPanel (palette);
         this.sectorPanel = new SectorPanel(palette);
         this.linePanel   = new LinePanel  ();
         this.sidePanel   = new SidePanel  (palette);
@@ -59,6 +59,8 @@ public class LevelViewer extends JFrame {
 
         pack();
         setVisible(true);
+
+        levelPanel.show(level);
     }
 
 
